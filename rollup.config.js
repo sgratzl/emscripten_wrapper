ca0291dd606dbee3bfbac3553ba9103b481bd831
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-06-26 10:06:03 +1000

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+// rollup.config.js
+import typescript from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+
+export default [{
+  input: './src/index.ts',
+  output: [
+    {
+      file: 'build/index.js',
+      exports: 'named',
+      sourcemap: true,
+      format: 'cjs'
+    },
+    {
+      file: 'build/index.mjs',
+      exports: 'named',
+      sourcemap: true,
+      format: 'esm'
+    }
+  ],
+  external: ['events'],
+  plugins: [
+    resolve({
+      preferBuiltins: true
+    }),
+    typescript()
+	]
+}, {
+  input: './src/index.ts',
+  output: {
+    file: 'build/index.umd.js',
+    name: 'EMScriptenWrapper',
+    exports: 'named',
+    format: 'umd'
+  },
+  plugins: [
+    builtins(),
+    resolve({
+      preferBuiltins: false
+    }),
+		typescript()
+	]
+}]
